a3268ca681394cf0d166151f2f55f6d7c2a5e23f
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2024-01-14 22:12:12 -0500

    Add todo app.

---
+++ supertask/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+import Block from './components/Block.vue'
+
+let id = -1;
+
+let supertasks = ref([
+  {
+    id: ++id,
+    txt: `supertask text ${id}`,
+  },
+  {
+    id: ++id,
+    txt: `supertask text ${id}`,
+  },
+])
+
+function getTasks() {
+  // supertasks = localStorage.supertasks;
+}
+getTasks(); 
+
+</script>
+
+
+<template>
+  <main>
+    <div v-for="task in supertasks">
+      <Block :txt="task.txt" />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+}
+</style>
---
+++ supertask/src/components/Block.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { defineProps, ref } from 'vue';
+
+defineProps({
+    txt: ''
+})
+
+let taskDescription = ref('');
+</script>
+
+<template>
+    <div class="block">
+        {{ txt }}
+    </div>
+</template>
+
+<style scoped>
+    .block {
+        background-color: var(--color-02);
+        padding: 11rem;
+        margin: 1rem;
+        border-radius: 30px;
+    }
+</style>
