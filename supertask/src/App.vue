fb22662b6afd22f48475d2db19bd661c34f031a3
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2024-01-15 22:27:39 -0500

    Fix serialization.

--- supertask/src/App.vue
+++ supertask/src/App.vue
@@ -1,78 +1,48 @@
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue'
-import Block from './components/Block.vue'
+import { ref, onMounted, onUnmounted } from 'vue';
+import Block from './components/Block.vue';
 
 let id = -1;
 
-const tasks = ref([
-  // {
-  //   id: ++id,
-  //   name: `Learn`,
-  //   subtasks: [
-  //     { name: 'REST APIs', done: false },
-  //     { name: 'React', done: false },
-  //     { name: 'jQuery', done: false }
-  //   ]
-  // },
-  // {
-  //   id: ++id,
-  //   name: `Apply to jobs`,
-  // },
-  // {
-  //   id: ++id,
-  //   name: `Learn`,
-  //   subtasks: [
-  //     { name: 'REST APIs', done: false },
-  //     { name: 'React', done: false },
-  //     { name: 'jQuery', done: false }
-  //   ]
-  // },
-])
+const tasks = ref([]);
 
 function addTask() {
-  // if (tasks.value.some(x => x.name == 'New Task')) {
-  //   return
-  // }
-
-  tasks.value.push({ id: ++id, name: 'New Task', subtasks: [] })
+  tasks.value.push({ id: ++id, name: 'New Task', subtasks: [] });
+  saveTasks(); // Save tasks immediately after adding a new task
 }
 
 function saveTasks() {
-  console.log('tasks saved')
-  const parsed = JSON.stringify(tasks)
-  localStorage.setItem('tasks', parsed)
-
-  console.log('saved', parsed)
+  console.log('tasks saved');
+  const parsed = JSON.stringify(tasks.value);
+  localStorage.setItem('tasks', parsed);
+  console.log('saved', parsed);
 }
 
 onMounted(() => {
-  if (localStorage.getItem('tasks')) {
+  const storedTasks = localStorage.getItem('tasks');
+  if (storedTasks) {
     try {
-      tasks = JSON.parse(localStorage.getItem('tasks'))
-      } catch(e) {
-        console.log('Could not load from local storage')
-        // localStorage.removeItem('tasks')
-      }
+      tasks.value = JSON.parse(storedTasks);
+    } catch (e) {
+      localStorage.removeItem('tasks');
     }
-  })
-
-  onUnmounted(() => {
-  saveTasks();
+  }
 });
 
+onUnmounted(() => {
+  saveTasks();
+});
 </script>
-
 
 <template>
   <main>
-    <div v-for="task in tasks">
+    <div v-for="task in tasks" :key="task.id">
       <form @submit.prevent="saveTasks">
         <input v-model="task.name">
         <button>Save Task</button>
       </form>
       <Block :name="task.name" :subtasks="task.subtasks" />
     </div>
-    <!-- id < 0 ? 'Add your first task' :  -->
     <button @click="addTask">{{ '+' }}</button>
   </main>
 </template>
