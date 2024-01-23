2681bf57364e8b064774437608138ce95db9022e
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2024-01-22 20:27:18 -0500

    Get a list of current tasks.

--- supertask/src/components/FocusMode.vue
+++ supertask/src/components/FocusMode.vue
@@ -1,4 +1,5 @@
 <template>
+    {{ now }}
     <div v-for="task in tasksNow">
         <p class="task m-1">{{ task.name }}</p>
     </div>
@@ -12,17 +13,24 @@ const props = defineProps({
     tasks: Array
 })
 
+let now = ref(new Date())
+let tasksNow = ref([])
+
+setInterval(() => {
+    now.value = new Date()
+    tasksNow.value = getCurrentTasks()
+}, 1000);
+
 
 function isHappeningNow(task) {
-    const timeNow = Date.now()
-    return task.timeSpan[0] < timeNow && timeNow < task.timeSpan[1]
+    const currentTime = now.value.getHours().toString().padStart(2, '0') + ':' + now.value.getMinutes().toString().padStart(2, '0');
+    console.log('timeNow', currentTime, 'task.timeSpan.start', task.timeSpan.start, 'task.timeSpan.end', task.timeSpan.end)
+    return task.timeSpan.start <= currentTime && currentTime <= task.timeSpan.end
 }
 
 function getCurrentTasks() {
-    console.log('tasks', props.tasks[0])
-    console.log('tasks', props.tasks[0])
-    // tasks.filter((t) => isHappeningNow(t))
+    tasksNow = props.tasks.filter((t) => isHappeningNow(t))
+    console.log('tasksNow', tasksNow)
 }
 
-getCurrentTasks()
 </script>
