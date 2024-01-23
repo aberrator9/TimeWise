6448d3b28d5acd238cbb2e52052fd85670084035
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2024-01-23 12:13:06 -0500

    Show remaining time.

--- supertask/src/components/FocusMode.vue
+++ supertask/src/components/FocusMode.vue
@@ -1,9 +1,11 @@
 <template>
-    <div v-for="task in tasksNow">
+    <div class="container" v-for="task in tasksNow">
         <p class="task m-1">{{ task.name }}</p>
+        <p class="align-center">For {{ getRemaining(task) }} more {{ units }}</p>
         <div class="container">
             <p class="subtask m-1"> {{ (task.subtasks && task.subtasks.length) ? task.subtasks[displayedIdx].name : '' }}</p>
-            <button @click="rerollSubtask(task.subtasks.length)" class="align-center m-1 die" :class="{ hidden: !task.subtasks.length }"><img id="button-img" src="../assets/die.svg"></button>
+            <button @click="rerollSubtask(task.subtasks.length)" class="align-center m-1 die"
+                :class="{ hidden: !task.subtasks.length }"><img id="button-img" src="../assets/die.svg"></button>
         </div>
     </div>
 </template>
@@ -18,7 +20,7 @@ const props = defineProps({
 let now = ref(new Date())
 let tasksNow = ref([])
 const displayedIdx = ref(0)
-const hidden = ref(false)
+let units = 'hours'
 
 setInterval(() => {
     now.value = new Date()
@@ -29,24 +31,47 @@ onMounted(() => tasksNow.value = getCurrentTasks())
 
 function isHappeningNow(task) {
     const today = now.value.getDay()
-    console.log('today', today, 'task.days[thisDay]', task.days[today])
-    const currentTime = now.value.getHours().toString().padStart(2, '0') + ':' + now.value.getMinutes().toString().padStart(2, '0');
+    const time = HHMM(now.value);
+
+    // console.log('today', today, 'task.days[thisDay]', task.days[today])
     // console.log('timeNow', currentTime, 'task.timeSpan.start', task.timeSpan.start, 'task.timeSpan.end', task.timeSpan.end)
-    return task.days[today] === true && (task.timeSpan.start <= currentTime && currentTime <= task.timeSpan.end)
+
+    if (task.timeSpan.start <= task.timeSpan.end) {     // Doesn't go over midnight
+        return task.days[today] && (task.timeSpan.start <= time && time <= task.timeSpan.end)
+    } else {
+        return (task.days[today] || task.days[today === 6 ? 0 : Math.min(today + 1, 6)]) && (task.timeSpan.start <= time || time <= task.timeSpan.end)
+    }
+}
+
+function HHMM(date) {
+    return date.getHours().toString().padStart(2, '0') + ':' + now.value.getMinutes().toString().padStart(2, '0')
 }
 
 function getCurrentTasks() {
     tasksNow = props.tasks.filter((t) => isHappeningNow(t))
 }
 
-function rerollSubtask(length){
+function getRemaining(task) {
+    const hours = task.timeSpan.end.split(':')[0] - HHMM(now.value).split(':')[0]
+    const minutes = task.timeSpan.end.split(':')[1] - HHMM(now.value).split(':')[1]
+    
+    if( hours > 0) {
+        units = 'hour' + (hours > 1 ? 's' : '')
+        return hours
+    } else {
+        units = 'minute' + (minutes > 1 ? 's' : '')
+        return minutes
+    }
+}
+
+function rerollSubtask(length) {
     if (length < 2) {
         return
     }
 
     let random = displayedIdx.value
 
-    while(random === displayedIdx.value) {
+    while (random === displayedIdx.value) {
         random = Math.floor(Math.random() * length)
     }
 
